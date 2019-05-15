5dd680a8c8c2633b2847ad221b9eb53936cad95a
jojinwook <whwlsdnr823@naver.com>
2019-05-15 23:57:05 +0900

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from flask import request, jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+class RegistUser(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('email', type=str)
+        args = parser.parse_args()
+
+        name = args['name']
+        email = args['email']
+
+        return {'name': name, 'email': email}
+
+
+api.add_resource(RegistUser, '/user')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
