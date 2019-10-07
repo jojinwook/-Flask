d836d8c764026cae7f4a6e0b5d4bfc08daac7881
jojinwook <whwlsdnr823@naver.com>
2019-10-07 23:43:33 +0900

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -27,3 +27,22 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
     
 ===============
+
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['myName']
+      return redirect(url_for('success', name=user))
+   else:
+      user = request.args.get('myName')
+      return redirect(url_for('success', name=user))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=
